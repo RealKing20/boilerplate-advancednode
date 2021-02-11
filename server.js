9611fdd21e4764a72dfedbe9f0e6983e0a7462e8
KING <abayemiracle@gmail.com>
2021-02-11 16:09:55 +0100

    added passport login route authentication added

--- server.js
+++ server.js
@@ -3,16 +3,118 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID
+const LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+//datbase
+
+
+fccTesting(app); 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//cookie session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+
+//passport
+app.use(passport.initialize())
+app.use(passport.session());
+
+app.use(express.static(__dirname + '/views'));
+
+//setting pug
+app.set('view engine', 'pug')
+
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+ res.render(__dirname + "/views/pug/index", {title: 'Hello', message: 'Please login'});
+});
+
+myDB(async client => {
+  const myDataBase = await client.db('kingdata').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+
+    res.render('pug',
+     {
+      title: 'Connected to Database',
+      message: 'Please login',    
+      showLogin: true,
+      showRegistration: true
+    });
+  });
+
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  });
+
+  //A middleware function to stop anyone from accessing the profile page using the URL  
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  };
+
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
+  });
+
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User '+ username +' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+
+
+ 
+
+
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
